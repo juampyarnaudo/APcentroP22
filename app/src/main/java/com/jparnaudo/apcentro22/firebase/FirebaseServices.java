f9d20cab3b4caccdd5d4d1794b229885c8a8e9e4
Juampy <iarnaudojuanpablo@gmail.com>
2020-05-15 19:24:33 -0300

    Firebase Not
    fixed

--- app/src/main/java/com/jparnaudo/apcentro22/firebase/FirebaseServices.java
+++ app/src/main/java/com/jparnaudo/apcentro22/firebase/FirebaseServices.java
@@ -1,24 +1,7 @@
 package com.jparnaudo.apcentro22.firebase;
 
-
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
-import android.content.Context;
-import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
 import android.util.Log;
-
-import androidx.annotation.NonNull;
-import androidx.core.app.NotificationCompat;
-
-import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessagingService;
-import com.google.firebase.messaging.RemoteMessage;
-import com.jparnaudo.apcentro22.AuthActivity;
-import com.jparnaudo.apcentro22.R;
 
 public class FirebaseServices extends FirebaseMessagingService {
 
--- app/src/main/java/com/jparnaudo/apcentro22/firebase/MiFirebaseInstanceIdService.java
+++
@@ -1,7 +0,0 @@
-package com.jparnaudo.apcentro22.firebase;
-
-import com.google.firebase.messaging.FirebaseMessagingService;
-
-public class MiFirebaseInstanceIdService extends FirebaseMessagingService {
-
-}
